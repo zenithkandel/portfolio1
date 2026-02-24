126ca17629b2c44bcfe5818776e488adcc5a91af
Zenith Kandel <kandelze123@gmail.com>
2026-02-24 15:12:11 +0545

    Remove smooth scroll functionality from the main script

--- assets/js/main.js
+++ assets/js/main.js
@@ -497,17 +497,6 @@ function initTime() {
     setInterval(update, 1000);
 }
 
-// Smooth scroll
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', e => {
-        e.preventDefault();
-        const target = document.querySelector(anchor.getAttribute('href'));
-        if (target) {
-            target.scrollIntoView({ behavior: 'smooth', block: 'start' });
-        }
-    });
-});
-
 // Contact form AJAX
 function initContactForm() {
     const form = document.getElementById('contactForm');
